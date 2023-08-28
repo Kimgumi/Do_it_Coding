6dd92382fda2b7dad51031a55e606fca3a76d51c
Kimgumi <apple220241@gmail.com>
2023-08-28 12:31:54 +0900

    CBV로 목록과 상세페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()), #CBV로 제공된 클래스인 ListView 사용
+    #FBV - path('', views.index),
+    #FBV - path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-def index(request):
-    #posts = Post.objects.all()
-    posts = Post.objects.all().order_by('-pk')   #최신글부터
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#CBV로 여러 포스트 나열 목록 웹페이지 만들기
+class PostList(ListView): #index함수 대체
+    model = Post
+    #ListView 클래스를 사용해 'model명_list.html'을 기본 템플릿으로 사용
+    template_name = 'blog/index.html'
+    ordering = '-pk'
+class PostDetail(DetailView): #single_post_page함수 대체
+    model = Post
+    template_name = 'blog/post_detail.html'
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#FBV로 페이지 제작시 방법
+# def index(request):
+#     #posts = Post.objects.all()
+#     posts = Post.objects.all().order_by('-pk')   #최신글부터
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
