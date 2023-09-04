b64a698ced44536fb87ae89d5e5c0578bddfa601
Kimgumi <apple220241@gmail.com>
2023-09-04 12:59:05 +0900

    author 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     content = models.TextField()
-
+    
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     file_upload = models.FileField(upload_to="blog/files//%Y/%m/%d", blank=True)
 
@@ -15,7 +17,7 @@ class Post(models.Model):
 
     # author : 타 테이블의 주요키이자 이 테이블에서 외래키
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ class PostList(ListView): #index함수 대체
     model = Post
     #ListView 클래스를 사용해 'model명_list.html'을 기본 템플릿으로 사용
     template_name = 'blog/post_list.html'
-    ordering = '-pk'
+    ordering = '-pk' # 최신순
 
 class PostDetail(DetailView): #single_post_page함수 대체
     model = Post
